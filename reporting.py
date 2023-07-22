b921fa81e90c27d00f8ef8ac7d59eb61cc3701e8
Roberto Chávez Trujillo <38190230+Rchatru@users.noreply.github.com>
2023-07-22 18:51:01 +0100

    Create reporting.py

---
+++ reporting.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+# Icon alojado en: https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f4ca.png
+# Ha sido necesario cambiar la ubicación del mismo ya que streamlit busca por defecto en maxcdn que ha sido cerrado
+# https://github.com/twitter/twemoji/issues/580
+
+st.set_page_config(
+    page_title="ML Reporting",
+    page_icon="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f4ca.png",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'Get Help': 'https://github.com/Rchatru/ml-reporting-app/',
+        'Report a bug': "https://github.com/Rchatru/ml-reporting-app/issues",
+        'About': "# ML Reporting Webapp. Roberto Chávez Trujillo."
+    }
+)
+
+st.title('ML Reporting')
